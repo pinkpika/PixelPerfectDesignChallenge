a04c3f479c6f810c5f4e3e788ad22b24b3e3754b
pink_pika <pink_pika@cmoney.net.tw>
2022-06-17 15:16:29 +0800

    feat: add ColorPicker, Show/Hide Spacing, CleanAll

--- PixelPerfectDesignChallenge/PixelPerfectDesignChallenge/ContentView.swift
+++ PixelPerfectDesignChallenge/PixelPerfectDesignChallenge/ContentView.swift
@@ -9,61 +9,80 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var spacing: CGFloat = 1
-    var pixelCount: Int = 16
-    var pixelWidth: CGFloat = 20
     var totalWidth: CGFloat {
-        return ( pixelWidth + spacing ) * CGFloat(pixelCount) - spacing
+        return UIScreen.main.bounds.size.width - 20.0
     }
+    var pixelWidth: CGFloat {
+        return ( totalWidth - spacing ) / CGFloat(pixelCountForSide) - spacing
+    }
+    
+    let pixelCountForSide: Int = 16
     
     @State var allColor: [Color] = Array(repeating: .white, count: 16*16)
-    
-    var columes: [GridItem] = Array(repeating: GridItem(spacing: 1), count: 16)
-    
-    @State var buttonTitle = "Drawing"
+    @State var drawingButtonTitle = "Drawing"
     @State var isDrawing = true
+    var spacingButtonTitle = "Show/Hide Spacing"
+    @State var spacing: CGFloat = 1
+    @State var bgColor = Color(.sRGB, red: 0.0, green: 0.0, blue: 0.0)
+    
+    var columes: [GridItem] {
+        return Array(repeating: GridItem(spacing: spacing), count: pixelCountForSide)
+    }
     
     var body: some View {
-        Button(buttonTitle, action: {
-            isDrawing = !isDrawing
-            buttonTitle = isDrawing ? "Drawing" : "Eraser"
-        })
-        LazyVGrid(columns: columes, spacing: 1){
-            ForEach(0...pixelCount*pixelCount-1, id: \.self) { i in
-                Rectangle()
-                    .fill(allColor[i])
-                    .frame(width: pixelWidth, height: pixelWidth, alignment: .center)
-            }
+        VStack{
+            Text("Challenge: Pixel perfect design 16 x 16")
+            HStack{
+                Button(drawingButtonTitle, action: {
+                    isDrawing = !isDrawing
+                    drawingButtonTitle = isDrawing ? "Drawing" : "Erasing"
+                })
+                ColorPicker("", selection: $bgColor).labelsHidden()
+                Spacer()
+                Button(spacingButtonTitle, action: {
+                    spacing = spacing == 1 ? 0 : 1
+                }).foregroundColor(.green)
+                Button("CleanAll", action: {
+                    allColor = Array(repeating: .white, count: 16*16)
+                }).foregroundColor(.red)
+            }.padding()
+            VStack{
+                LazyVGrid(columns: columes, spacing: spacing){
+                    ForEach(0...pixelCountForSide*pixelCountForSide-1, id: \.self) { i in
+                        Rectangle()
+                            .fill(allColor[i])
+                            .frame(width: pixelWidth, height: pixelWidth, alignment: .center)
+                    }
+                }
+                .background(Color.gray)
+                .frame(width: totalWidth, height: totalWidth, alignment: .center)
+                .gesture(drag)
+            }.padding(5.0)
+            .background(Color.gray)
         }
-        .background(Color.gray)
-        .frame(width: totalWidth, height: totalWidth, alignment: .center)
-        .gesture(drag)
     }
     
-    @State var isDragging = false
-
+    /// 拖拉手勢
     var drag: some Gesture {
-        DragGesture()
+        DragGesture(minimumDistance: 0.1)
             .onChanged { value in
-                self.isDragging = true
-                print("ccc l \(value.location)")
                 if let drawIndex = locationToIndex(location: value.location),
                     self.allColor.indices.contains(drawIndex){
-                    self.allColor[drawIndex] = isDrawing ? .black : .white
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        self.allColor[drawIndex] = isDrawing ? bgColor : .white
+                    }
                 }
             }
-            .onEnded { _ in
-                self.isDragging = false
-                print("eee")
-            }
+            .onEnded { _ in }
     }
     
+    /// 取得Index
     func locationToIndex(location: CGPoint) -> Int?{
         guard location.x >= 0, location.y >= 0,
             location.x < totalWidth, location.y < totalWidth else { return nil }
         let indexX = Int(location.x / (pixelWidth+spacing))
         let indexY = Int(location.y / (pixelWidth+spacing))
-        return indexY * pixelCount + indexX
+        return indexY * pixelCountForSide + indexX
     }
 }
 
